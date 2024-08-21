30529f728a7b892783a82141e62e71daea81203c
gsnascimento <guigui.nascimento2002@gmail.com>
2024-08-21 19:52:56 -0300

    lista simples com função

---
+++ selecao/exercicio_03.py
@@ -0,0 +1,4 @@
+'''Escreva um programa para ler as notas das duas avaliações de um aluno no semestre, calcular e
+ escrever a média semestral e a seguinte mensagem: PARABÉNS! Você foi aprovado! somente se o
+ aluno foi aprovado (considere 6.0 a média mínima para aprovação)'''
+
