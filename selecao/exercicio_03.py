1f6d6877f763227539e1ced9cc42dad83bc04f9e
gsnascimento <guigui.nascimento2002@gmail.com>
2024-08-21 21:14:04 -0300

    exercicios 03 e 04 lista selecao

--- selecao/exercicio_03.py
+++ selecao/exercicio_03.py
@@ -2,3 +2,11 @@
  escrever a média semestral e a seguinte mensagem: PARABÉNS! Você foi aprovado! somente se o
  aluno foi aprovado (considere 6.0 a média mínima para aprovação)'''
 
+
+notaAv1 = float(input("Qual a primeira nota? "))
+notaAv2 = float(input("Qual a segunda nota? "))
+def calculoMedia ():
+    mediaSemestral = (notaAv1 + notaAv2) / 2
+    if mediaSemestral > 6.0:
+     return 'PARABÉNS! Você foi aprovado!'
+print(calculoMedia())
--- selecao/exercicio_04.py
+++ selecao/exercicio_04.py
@@ -1,2 +1,13 @@
 ''' Acrescente ao exercício anterior a mensagem Você foi REPROVADO! Estude mais... caso a
  média calculada seja menor que 6.0.'''
+
+
+notaAv1 = float(input("Qual a primeira nota? "))
+notaAv2 = float(input("Qual a segunda nota? "))
+def calculoMedia ():
+    mediaSemestral = (notaAv1 + notaAv2) / 2
+    if mediaSemestral > 6.0:
+     return 'PARABÉNS! Você foi aprovado!'
+    else:
+     return 'Você foi REPROVADO! Estude mais...'
+print(calculoMedia())      
